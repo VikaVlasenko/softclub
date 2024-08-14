8de870b5d1a3e174df87c7d066eb765797157e35
VikaVlasenko <vikavvv2006.333@gmail.com>
2024-08-14 09:26:58 +0300

    1

--- vvv/src/main/java/com/softclub/vvv/VvvApplication.java
+++ vvv/src/main/java/com/softclub/vvv/VvvApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VvvApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VvvApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(VvvApplication.class, args);
 	}
 
 }
